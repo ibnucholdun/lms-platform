24890268471f6115c8f64b4234ddd74d2265c83d
ibnucholdun <ibnucholdun023@gmail.com>
2024-04-02 12:12:26 +0700

    feat: reorder functionality chapter

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/ChaptersForm.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/ChaptersForm.tsx
@@ -7,7 +7,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
-import { PlusCircle } from "lucide-react";
+import { Loader2, PlusCircle } from "lucide-react";
 
 import {
   Form,
@@ -20,6 +20,7 @@ import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { Chapter, Course } from "@prisma/client";
 import { Input } from "@/components/ui/input";
+import ChapterList from "./ChapterList";
 
 type Props = {
   initialData: Course & { chapters: Chapter[] };
@@ -57,8 +58,34 @@ const ChaptersForm: React.FC<Props> = ({ initialData, courseId }) => {
     }
   };
 
+  const onReorder = async (updateData: { id: string; position: number }[]) => {
+    try {
+      setIsUpdating(true);
+
+      await axios.put(`/api/courses/${courseId}/chapters/reorder`, {
+        list: updateData,
+      });
+
+      toast.success("Chapters reordered successfully");
+      router.refresh();
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setIsUpdating(false);
+    }
+  };
+
+  const onEdit = (id: string) => {
+    router.push(`/teacher/courses/${courseId}/chapters/${id}`);
+  };
+
   return (
-    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+    <div className=" mt-6 border bg-slate-100 rounded-md p-4 relative">
+      {isUpdating && (
+        <div className="absolute h-full w-full bg-slate-500/20 top-0 right-0 rounded-md flex items-center justify-center">
+          <Loader2 className="w-6 h-6 animate-spin text-sky-700" />
+        </div>
+      )}
       <div className="font-medium flex items-center justify-between">
         Course Chapters
         <Button variant="ghost" onClick={toggleCreating}>
@@ -106,7 +133,11 @@ const ChaptersForm: React.FC<Props> = ({ initialData, courseId }) => {
             !initialData.chapters.length && "text-slate-500 italic"
           )}>
           {!initialData.chapters.length && <>No chapters</>}
-          {/* TODO: Add a list of the chapters */}
+          <ChapterList
+            onEdit={onEdit}
+            onReorder={onReorder}
+            items={initialData.chapters || []}
+          />
         </div>
       )}
       {!isCreating && (

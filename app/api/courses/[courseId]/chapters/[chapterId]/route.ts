c3fe6c9bdc9f29db7e7e8cb058525a008d818300
ibnucholdun <ibnucholdun023@gmail.com>
2024-04-02 12:58:33 +0700

    feat: chapter edit page UI

---
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -0,0 +1,45 @@
+import prisma from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { courseId: string; chapterId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const { isPublished, ...values } = await req.json();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const courseOwner = await prisma.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId,
+      },
+    });
+
+    if (!courseOwner) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const chapter = await prisma.chapter.update({
+      where: {
+        id: params.chapterId,
+        courseId: params.courseId,
+      },
+      data: {
+        ...values,
+      },
+    });
+
+    // TODO: upload video url
+
+    return NextResponse.json(chapter);
+  } catch (error) {
+    console.log("[COURSES_ID]", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}

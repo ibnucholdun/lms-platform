b39bfd7f677c220dbed9a9e969d5e73adb274e41
ibnucholdun <ibnucholdun023@gmail.com>
2024-04-03 14:52:34 +0700

    feat: course publish actions

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/Actions.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import ConfirmModal from "@/components/modals/ConfirmModal";
+import { Button } from "@/components/ui/button";
+import { useConfettiStore } from "@/hooks/useConfettiStore";
+import axios from "axios";
+import { Trash } from "lucide-react";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
+
+type Props = {
+  disabled?: boolean;
+  courseId: string;
+  isPublished: boolean;
+};
+
+const Actions: React.FC<Props> = ({ disabled, courseId, isPublished }) => {
+  const router = useRouter();
+  const confetti = useConfettiStore();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onPublish = async () => {
+    try {
+      setIsLoading(true);
+
+      if (isPublished) {
+        await axios.patch(`/api/courses/${courseId}/unpublish`);
+        toast.success("Course unpublished successfully");
+      } else {
+        await axios.patch(`/api/courses/${courseId}/publish`);
+        toast.success("Course published successfully");
+        confetti.onOpen();
+      }
+      router.refresh();
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const onDelete = async () => {
+    try {
+      setIsLoading(true);
+      await axios.delete(`/api/courses/${courseId}`);
+      toast.success("Course deleted successfully");
+      router.push(`/teacher/courses`);
+      router.refresh();
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="flex items-center gap-x-2">
+      <Button
+        onClick={onPublish}
+        disabled={disabled || isLoading}
+        variant="outline"
+        size="sm">
+        {isPublished ? "Unpublish" : "Publish"}
+      </Button>
+      <ConfirmModal onComfirm={onDelete}>
+        <Button size="sm" disabled={isLoading}>
+          <Trash className="h-4 w-4" />
+        </Button>
+      </ConfirmModal>
+    </div>
+  );
+};
+
+export default Actions;
--- app/api/courses/[courseId]/route.ts
+++ app/api/courses/[courseId]/route.ts
@@ -1,6 +1,12 @@
 import prisma from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
+import Mux from "@mux/mux-node";
+
+const { video } = new Mux({
+  tokenId: process.env.MUX_TOKEN_ID!,
+  tokenSecret: process.env.MUX_TOKEN_SECRET!,
+});
 
 export async function PATCH(
   req: Request,
@@ -28,6 +34,55 @@ export async function PATCH(
     return NextResponse.json(course);
   } catch (error) {
     console.log("[COURSES_ID]", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { courseId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const { courseId } = params;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const course = await prisma.course.findUnique({
+      where: {
+        id: courseId,
+        userId,
+      },
+      include: {
+        chapters: {
+          include: {
+            muxData: true,
+          },
+        },
+      },
+    });
+
+    if (!course) {
+      return new NextResponse("Not Found", { status: 404 });
+    }
+
+    for (const chapter of course.chapters) {
+      if (chapter.muxData?.assetId) {
+        await video.assets.delete(chapter.muxData.assetId);
+      }
+    }
+
+    const deletedCourse = await prisma.course.delete({
+      where: {
+        id: courseId,
+      },
+    });
+
+    return NextResponse.json(deletedCourse);
+  } catch (error) {
+    console.log("[COURSES_ID_DELETE]", error);
     return new NextResponse("Internal Server Error", { status: 500 });
   }
 }

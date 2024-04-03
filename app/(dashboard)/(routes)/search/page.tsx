76f176bb746f424989279cb935c5e9d51f76b589
ibnucholdun <ibnucholdun023@gmail.com>
2024-04-03 22:25:51 +0700

    feat: category query in search

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,7 +1,18 @@
+import prisma from "@/lib/db";
 import React from "react";
+import Categories from "./_components/Categories";
 
-const SearchPage = () => {
-  return <div>SearchPage</div>;
+const SearchPage = async () => {
+  const categories = await prisma.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
+  return (
+    <div className="p-6 ">
+      <Categories items={categories} />
+    </div>
+  );
 };
 
 export default SearchPage;

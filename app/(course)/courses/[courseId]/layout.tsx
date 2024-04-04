246ac3be5c28b0e19f017f05c0c1cfcf2279badb
ibnucholdun <ibnucholdun023@gmail.com>
2024-04-04 12:49:24 +0700

    feat: course & chapter layout

---
+++ app/(course)/courses/[courseId]/layout.tsx
@@ -0,0 +1,59 @@
+import getProgress from "@/actions/getProgress";
+import prisma from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import React from "react";
+import CourseSidebar from "./_components/CourseSidebar";
+import CourseNavbar from "./_components/CourseNavbar";
+
+type Props = {
+  children: React.ReactNode;
+  params: { courseId: string };
+};
+
+const CourseLayout: React.FC<Props> = async ({ children, params }) => {
+  const { userId } = auth();
+
+  if (!userId) return redirect("/");
+
+  const course = await prisma.course.findUnique({
+    where: {
+      id: params.courseId,
+    },
+    include: {
+      chapters: {
+        where: {
+          isPublished: true,
+        },
+        include: {
+          userProgress: {
+            where: {
+              userId,
+            },
+          },
+        },
+        orderBy: {
+          position: "asc",
+        },
+      },
+    },
+  });
+
+  if (!course) return redirect("/");
+
+  const progressCount = await getProgress(userId, course.id);
+
+  return (
+    <div className="h-full">
+      <div className="h-[80px] md:pl-80 fixed inset-y-0 w-full z-50">
+        <CourseNavbar course={course} progressCount={progressCount} />
+      </div>
+      <div className="hidden md:flex h-full w-80 flex-col fixed inset-y-0 z-50">
+        <CourseSidebar course={course} progressCount={progressCount} />
+      </div>
+      <main className="md:pl-80 pt-[80px] h-full">{children}</main>
+    </div>
+  );
+};
+
+export default CourseLayout;
---
+++ app/(course)/courses/[courseId]/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const CourseIdPage = (props: Props) => {
+  return <div>CourseIdPage</div>;
+};
+
+export default CourseIdPage;
